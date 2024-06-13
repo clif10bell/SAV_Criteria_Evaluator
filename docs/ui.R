726cec64b174c47295a3171a09d8c5816d7ed58a
clif10bell <125143299+clif10bell@users.noreply.github.com>
2024-06-13 14:45:51 -0400

    Update ui.R

--- docs/ui.R
+++ docs/ui.R
@@ -1,5 +1,5 @@
 #
-# This is the user-interface definition of a Shiny web application. You can
+# This white is the user-interface definition of a Shiny web application. You can
 # run the application by clicking 'Run App' above.
 #
 # Find out more about building applications with Shiny here:
